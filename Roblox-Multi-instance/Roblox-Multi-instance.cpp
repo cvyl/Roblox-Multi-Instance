8e47f90d8a8d490c3120443f5f70e9f0a3898dad
user <no>
2023-10-03 13:07:53 +0200

    removing stuff that didnt work cos dont wanna fix

--- Roblox-Multi-instance/Roblox-Multi-instance.cpp
+++ Roblox-Multi-instance/Roblox-Multi-instance.cpp
@@ -4,8 +4,6 @@
 
 int main()
 {
-    // Set console title using escape sequence.
-    std::cout << "\033]0;" << "Roblox Mutliple Instance" << "\007";
     std::cout << "Please close all Roblox instances before running this program for the best results!\n";
     CreateMutexA(NULL, TRUE, "ROBLOX_singletonMutex");
     std::cout << "If you want to quit, please press enter or close the application.\n";
